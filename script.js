dfbf2ef2b04286068b484b5ba7b6c2125dac815b
kale-source <kaikychimbum11@gmail.com>
2024-05-27 01:31:54 -0300

    Atualização Final

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-// EVENTO DE TROCAR COR DO SITE
+// TROCAR COR DO SITE
 
-    document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
+    const themeSystem = localStorage.getItem("themeSystem") || "light";
+    document.documentElement.setAttribute('data-theme', themeSystem);
+    const trocarcor = document.getElementById("imagemdark");
+    const trocarlogo = document.getElementById("logo");
+    let lightMode = themeSystem === "light";
 
-      const trocarcor = document.getElementById("imagemdark")
-      let lightMode = false;
+    trocarcor.addEventListener("click", function () {
+        let oldTheme = localStorage.getItem("themeSystem") || "light";
+        let newTheme = oldTheme === "light" ? "dark" : "light";
 
-      trocarcor.addEventListener("click", function (){
-          if (lightMode) {
-          trocarcor.src = "images/Modo Dark.png";
-          } 
-          
-          else {
-              trocarcor.src = "images/Modo Light.png";
-          }
-          lightMode = !lightMode;
-      })
+        // Troca a imagem do botão
+        if (lightMode) {
+            trocarcor.src = "images/Modo Light.png";
+            trocarlogo.src = "images/Ecopolis Logo White.png";
+        } else {
+            trocarcor.src = "images/Modo Dark.png";
+            trocarlogo.src = "images/Ecopolis Logo.png";
+        }
+        lightMode = !lightMode;
 
-  })
+        // Atualiza o tema no documento e no armazenamento local
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem("themeSystem", newTheme);
+    });
+});
